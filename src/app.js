87705c76c39f45690205b3e1a4d6607b5e1645d4
Sallai András <andrewteki@gmail.com>
2023-03-15 10:13:43 +0100

    Következő verzió

--- src/app.js
+++ src/app.js
@@ -1,39 +1,60 @@
-
+const doc = {
+    calcButton: document.querySelector('#calcButton'),
+    deleteButton: document.querySelector('#deleteButton'),
+    baseInput: document.querySelector('#base'),
+    heightInput: document.querySelector('#height'),
+    areaInput: document.querySelector('#area'),
+    areaDiv: document.querySelector('#areadiv')
+};
 const state = {
     base: 0,
     height: 0,
-    area: 0
+    area: 0,
+    areaInputVisible: false
 };
 
 window.addEventListener('load', () => {
     init();
 });
 
-function init() {
-    const calcButton = document.querySelector('#calcButton');
-    calcButton.addEventListener('click', () => {
+function init() {    
+    doc.calcButton.addEventListener('click', () => {
         getInputData();
         startCalculation();
         render();
     });
+    doc.deleteButton.addEventListener('click', () => {
+        state.areaInputVisible = false;
+        render();
+    });
+    render();
 }
 
 function getInputData() {
-    const baseInput = document.querySelector('#base');
-    const heightInput = document.querySelector('#height');
-    state.base = Number(baseInput.value);
-    state.height = Number(heightInput.value);
+    state.base = Number(doc.baseInput.value);
+    state.height = Number(doc.heightInput.value);
 }
 
 function startCalculation() {
-    state.area = calcTriangleArea(state.base, state.height);    
+    state.area = calcTriangleArea(state.base, state.height);
+    state.areaInputVisible = true;
 }
 
 function calcTriangleArea(base, height) {
     return base * height / 2;
 }
 
-function render() {
-    const areaInput = document.querySelector('#area');
-    areaInput.value = state.area;
+function render() {    
+    if(state.areaInputVisible) {
+        doc.areaInput.value = state.area;
+        doc.areaDiv.style.display = 'block';
+    }else {
+        doc.areaDiv.style.display = 'none';
+        clearInputData();
+    }
+}
+
+function clearInputData() {
+    doc.baseInput.value = '';
+    doc.heightInput.value = '';
 }
